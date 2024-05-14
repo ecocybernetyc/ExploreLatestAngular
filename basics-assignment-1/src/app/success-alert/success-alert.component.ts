6651038dfea9c72e805d586187d8ec24517c082e
Ivan Manzhula <ecocybernetyc@gmail.com>
2024-05-14 20:20:31 +0200

    Assignment 1 Implementation

---
+++ basics-assignment-1/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-success-alert',
+    template: `
+        <p>This is a success message, hooray!</p>
+    `,
+    styles: ['p { color: #0c8825; font-weight: 600 }']
+})
+export class SuccessAlertComponent {
+
+}
